1a603ba0e6e007bf1da99cb284e6797cd4284782
Anthony Garza <anthony.garza@pruvan.com>
2020-04-11 10:40:59 -0500

    Add arrow keys and layout switching

--- keyboards/ergo101/keymaps/default/keymap.c
+++ keyboards/ergo101/keymaps/default/keymap.c
@@ -22,6 +22,12 @@ enum layer_names {
     _FN
 };
 
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  COLEMAK
+};
+
+
 #define LOCK LGUI(LCTL(KC_Q))
 #define __ KC_TRNS
 
@@ -33,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,  KC_W,  KC_F,  KC_P,  KC_G,             KC_J,  KC_L,  KC_U,  KC_Y,  KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,    KC_P7, KC_P8, KC_P9, KC_PPLS,
         KC_CAPS, KC_A,  KC_R,  KC_S,  KC_T,  KC_D,             KC_H,  KC_N,  KC_E,  KC_I,  KC_O,    KC_QUOT, KC_ENT,             KC_P4, KC_P5, KC_P6,
         KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,       MO(_FN), KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),        KC_P1, KC_P2, KC_P3, KC_PENT,
-        KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC,  KC_BSPC,          KC_SPC,           KC_RALT, KC_RGUI, KC_RCTL, KC_MUTE,             KC_P0, KC_P0, KC_PDOT
+        KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC,  KC_BSPC,          KC_SPC,           KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,             KC_P0, KC_P0, KC_PDOT
     ),
     /* Qwerty */
     [_QWERTY] = LAYOUT(
@@ -42,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,             KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,    KC_P7, KC_P8, KC_P9, KC_PPLS,
         KC_CAPS, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,             KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN, KC_QUOT, KC_ENT,             KC_P4, KC_P5, KC_P6,
         KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,       MO(_FN), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),        KC_P1, KC_P2, KC_P3, KC_PENT,
-        KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC,  KC_BSPC,          KC_SPC,           KC_RALT, KC_RGUI, KC_RCTL, KC_MUTE,             KC_P0, KC_P0, KC_PDOT
+        KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC,  KC_BSPC,          KC_SPC,           KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,             KC_P0, KC_P0, KC_PDOT
     ),
     [_FN] = LAYOUT(
-        RESET,         __, __, __, __, __,     __, __, __, __, __,  __, __,               __, __, __, LOCK,
+        RESET,  QWERTY, COLEMAK, __, __, __,     __, __, __, __, __,  __, __,            __, __, __, LOCK,
         __,  __,  __,  __,  __,  __,  __,      __,  __,  __,  __,  __, __, __, __,     __, __, __, __,
         __,  __,  __,  __,  __,  __,             __,  __,  __,  __,  __, __, __, __,   __, __, __, __,
         __, __,  __,  __,  __,  __,             __,  __,  __,  __,  __,    __, __,     __, __, __,
@@ -55,6 +61,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_QWERTY);
+            }
+            return false;
+            break;
+        case COLEMAK:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_BASE);
+            }
+            return false;
+        break;
+    }
     return true;
 }
 
