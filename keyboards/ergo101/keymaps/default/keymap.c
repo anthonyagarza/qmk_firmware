8f0babdcb909a0f31d808a1908aebd8aa75fadd6
Anthony Garza <anthony.garza@pruvan.com>
2020-04-22 17:24:55 -0500

    Replace the left space with gui

--- keyboards/ergo101/keymaps/default/keymap.c
+++ keyboards/ergo101/keymaps/default/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,  KC_W,  KC_F,  KC_P,  KC_G,             KC_J,  KC_L,  KC_U,  KC_Y,  KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC,    KC_P7, KC_P8, KC_P9, KC_PPLS,
         KC_CAPS, KC_A,  KC_R,  KC_S,  KC_T,  KC_D,             KC_H,  KC_N,  KC_E,  KC_I,  KC_O,    KC_QUOT, KC_ENT,             KC_P4, KC_P5, KC_P6,
         KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,       MO(_FN), KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),        KC_P1, KC_P2, KC_P3, KC_PENT,
-        KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC,  KC_BSPC,          KC_SPC,           KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,             KC_P0, KC_P0, KC_PDOT
+        KC_LCTL, KC_LGUI, KC_LALT,  KC_LGUI,  KC_BSPC,          KC_SPC,           KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,             KC_P0, KC_P0, KC_PDOT
     ),
     /* Qwerty */
     [_QWERTY] = LAYOUT(
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,             KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,    KC_P7, KC_P8, KC_P9, KC_PPLS,
         KC_CAPS, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,             KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN, KC_QUOT, KC_ENT,             KC_P4, KC_P5, KC_P6,
         KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,       MO(_FN), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),        KC_P1, KC_P2, KC_P3, KC_PENT,
-        KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC,  KC_BSPC,          KC_SPC,           KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,             KC_P0, KC_P0, KC_PDOT
+        KC_LCTL, KC_LGUI, KC_LALT,  KC_LGUI,  KC_BSPC,          KC_SPC,           KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,             KC_P0, KC_P0, KC_PDOT
     ),
     [_FN] = LAYOUT(
         RESET,  QWERTY, COLEMAK, __, __, __,     __, __, __, __, __,  __, __,            __, __, __, LOCK,
